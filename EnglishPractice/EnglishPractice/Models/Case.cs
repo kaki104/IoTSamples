f8fcd4234bf0e825a8874a2b7e1397e49bc1a369
kaki104 <kaki104@hanmail.net>
2018-01-08 12:22:53 +0900

    #2 #3 프로젝트 추가와 파일 읽어서 초기화 하기 작업 완료

---
+++ EnglishPractice/EnglishPractice/Models/Case.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishPractice.Models
+{
+    /// <summary>
+    /// 케이스
+    /// </summary>
+    public class Case
+    {
+        public int Index { get; set; }
+        public string ShowText { get; set; }
+        public string SpeakText { get; set; }
+    }
+}
