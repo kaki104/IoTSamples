177a2bdc75268334f18ca0d23fcc3067d829b4e8
kaki104 <kaki104@hanmail.net>
2018-01-08 16:11:08 +0900

    #4 기초 연습 페이지 구현

---
+++ EnglishPractice/EnglishPractice/Helpers/CommonHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishPractice.Helpers
+{
+    public static class CommonHelper 
+    {
+        public static async Task ShowMessageAsync(string message, string title = "Information")
+        {
+            var messageDialog = new Windows.UI.Popups.MessageDialog(message, title);
+            await messageDialog.ShowAsync();
+        }
+    }
+}
