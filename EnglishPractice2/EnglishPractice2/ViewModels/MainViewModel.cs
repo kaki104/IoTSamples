2ec1ccb85a820ff2dae0ca3eecff6d0544b55222
kaki104 <kaki104@hanmail.net>
2018-01-12 12:33:06 +0900

    #11 단문 연습 앱 V2 추가

---
+++ EnglishPractice2/EnglishPractice2/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using GalaSoft.MvvmLight;
+
+namespace EnglishPractice2.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            
+        }
+    }
+}
