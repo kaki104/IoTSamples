4946e0c4ab6ac4bdf6713f300a4cff82296c456f
kaki104 <kaki104@hanmail.net>
2018-01-18 16:33:02 +0900

    #22 add iot player project

---
+++ IoTPlayer/IoTPlayer/Views/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using IoTPlayer.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace IoTPlayer.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        private MainViewModel ViewModel
+        {
+            get { return DataContext as MainViewModel; }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ IoTPlayer/IoTPlayer/Views/ShellPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using IoTPlayer.Services;
+using IoTPlayer.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace IoTPlayer.Views
+{
+    public sealed partial class ShellPage : Page
+    {
+        private ShellViewModel ViewModel
+        {
+            get { return DataContext as ShellViewModel; }
+        }
+
+        public ShellPage()
+        {
+            InitializeComponent();
+            DataContext = ViewModel;
+            ViewModel.Initialize(shellFrame);
+        }
+    }
+}
